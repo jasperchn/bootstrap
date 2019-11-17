31e343d67dcfb228e03c08d70666e577c71f120a
xin763553265 <31581883+xin763553265@users.noreply.github.com>
2019-11-17 00:27:32 -0600

    Add files via upload

--- homework10.py
+++ homework10.py
@@ -10,38 +10,12 @@ from homework_9_4 import *
 import numpy
 from pandas import read_csv
 from sklearn.utils import resample
+import scipy
 from scipy import stats
 # from sklearn.tree import DecisionTreeClassifier
 # from sklearn.metrics import accuracy_score
 
 
-# def data_restructure():
-# 	# in this function, I will restructure the dataframe, I will add 
-# 	# two indicator to indicate whether it is a product 1 or product 2
-# 	xls = pd.ExcelFile('purchase.xlsx')
-# 	purchase_data = pd.read_excel(xls, 'Purchase_data')
-# 	purchase_data = purchase_data.rename(columns={"Purchase Product 1":"Purchase1","log(price of product 1)":"price1",\
-# 		"Purchase Product 2":"Purchase2","log(price of product 2)":"price2"})
-# 	# purchase_1 = purchase_data[["Purchase Product 1","log(price of product 1)"]]
-# 	# purchase_1 = purchase_1.rename(columns={"Purchase Product 1":"Purchase","log(price of product 1)":"price"})
-# 	# purchase_1["product1"] = 1
-# 	# purchase_1["product2"] = 0
-# 	# purchase_2 = purchase_data[["Purchase Product 2","log(price of product 2)"]]
-# 	# purchase_2 = purchase_2.rename(columns={"Purchase Product 2":"Purchase","log(price of product 2)":"price"})
-# 	# purchase_2["product1"] = 0
-# 	# purchase_2["product2"] = 1
-# 	# purchase_data_new = pd.concat([purchase_1,purchase_2])
-# 	# purchase_data_new.to_csv("purchase_new.csv")
-	
-# 	purchase_data["purchase_both"] = purchase_data["Purchase1"]*purchase_data["Purchase2"]
-# 	purchase_data["purchase_1_only"] = purchase_data["Purchase1"]*(1-purchase_data["Purchase2"])
-# 	purchase_data["purchase_2_only"] = (1-purchase_data["Purchase1"])*purchase_data["Purchase2"]
-# 	purchase_data["no_purchase"] = (1-purchase_data["Purchase1"])*(1-purchase_data["Purchase2"])
-# 	purchase_data["purchase_identify"]= purchase_data.apply(lambda x: json.dumps([x["purchase_both"],x["purchase_1_only"],x["purchase_2_only"],x["no_purchase"]]),axis=1)
-# 	purchase_data["purchase_identify"] = purchase_data["purchase_identify"].apply(lambda x: json.loads(x))
-# 	# print(purchase_data[:5])
-
-# 	return purchase_data	
 
 def resample_data(df=data_restructure()):
 	df_truncated = df[["Purchase1","price1","Purchase2","price2","purchase_both","purchase_1_only","purchase_2_only","no_purchase"]]
@@ -88,20 +62,23 @@ def nonparams_bootstrap(n_boot_iter=99):
 		estimated_parameters = params_estimate_new()
 		estimated_parameters_list.append(estimated_parameters)
 	params_array = np.array(estimated_parameters_list)
-	print(params_array)
-	print(params_array.shape)
+	# print(params_array)
+	# print(params_array.shape)
 
 	return params_array
 
-# def params_bootstrap(n_boot_iter=99):
+def data_reparams():
+	return
+
+def params_bootstrap(n_boot_iter=99):
+	return
 
 
-
-def Calculate_se_CI(params_array,n_boot_iter=99,significance=0.9,boot_method = "non_params"):
+def Calculate_se_CI(params_array,n_boot_iter=99,significance=0.9,boot_method = "non_parametric_bootstrap"):
 	# params_df = nonparams_bootstrap(n_boot_iter)
-	params_df = pd.DataFrame({"alpha_1":params_array[:,0],"alpha_2":params_array[:,1],"lambda":params_array[:,2],"gamma21":params_array[:3]})
-	params_df.to_csv("nonparams_result.csv")
-	st_error = stats.sem(params_array)
+	params_df = pd.DataFrame({"alpha_1":params_array[:,0],"alpha_2":params_array[:,1],"lambda":params_array[:,2],"gamma21":params_array[:,3]})
+	params_df.to_csv(f"{boot_method}_result.csv")
+	st_error = scipy.stats.sem(params_array)
 	std = np.std(params_array,axis = 0)
 	print("standard_error",st_error)
 	params = [0.92, 0.44, -0.16, 0.31] # estimated params from homework9
@@ -109,18 +86,25 @@ def Calculate_se_CI(params_array,n_boot_iter=99,significance=0.9,boot_method = "
 	print(T_Zb.shape)
 	p_value_lower = ((1.0-significance)/2.0) * 100
 	p_value_upper = (significance+((1.0-significance)/2.0)) * 100
-	f = open(f'./{boot_method}_CI.txt','wb')
+	print("p_value_upper",p_value_upper)
+	file = open(f'./{boot_method}_CI.txt','wb')
 
 	for each_params in list(params_df.columns):
-		stats = T_Zb[each_params]
-		each_params_est = params[params.index(each_params)]
-		lower = numpy.percentile(stats, p_value_lower)
-		lower_bound = each_params_est- (1/np.sqrt(n_boot_iter)) * st_error * lower
-		upper = numpy.percentile(stats, p_value_upper)
-		upper_bound = each_params_est- (1/np.sqrt(n_boot_iter)) * st_error * upper
-		f.write('%.1f confidence interval for %.1f%% is%.1f%% and %.1f%%' % (significance*100, each_params, lower_bound, upper_bound))
+		params_stats = params_df[each_params]
+		each_params_est = params[list(params_df.columns).index(each_params)]
+		K_a = numpy.percentile(params_stats, p_value_upper)
+		print("K_a",K_a)
+		# lower = numpy.percentile(params_stats, p_value_lower)
+		lower_bound = each_params_est- (1/np.sqrt(n_boot_iter)) * st_error[list(params_df.columns).index(each_params)] * K_a
+		print("lower_bound",lower_bound)
+		# upper = numpy.percentile(params_stats, p_value_upper)
+		upper_bound = each_params_est + (1/np.sqrt(n_boot_iter)) * st_error[list(params_df.columns).index(each_params)] * K_a
+		print("upper_bound",upper_bound)
+		file.write(f'Under {boot_method}, confidence interval for {significance*100} is between {lower_bound} and {upper_bound}')
 
-	f.close()
+
+
+	file.close()
 
 
 
